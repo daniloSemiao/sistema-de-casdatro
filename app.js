88219a6c06d2b1b5d16885910304cb398dd2d0cf
SleezyX <102337590+SleezyX@users.noreply.github.com>
2023-10-16 14:55:43 -0300

    atualizando o sistema de jogos

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-console.loog("Rodando o sistema de gerenciamento de jogos")
+console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
