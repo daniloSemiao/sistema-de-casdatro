a702855baca6fcd1aaebe0107980ee844a8ccd88
Danilo Semiao Rigo <semiaorigo@gmail.com>
2023-10-30 14:46:11 -0300

    adicionando um console log no arquivo app.js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+console.log("O paulo fes esse comit sem voce ver na aula")
+console.log("enviando para o Github")
